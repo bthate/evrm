304e3a5c0a4c1d48f630cda7bda9239071cc48de
Bart Thate <bthate67@gmail.com>
2021-09-06 23:40:42 +0200

    move to package

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read():
 
 setup(
     name='evrm',
-    version='77',
+    version='78',
     url='https://github.com/bthate/evrm',
     author='Bart Thate',
     author_email='bthate67@gmail.com',
@@ -36,8 +36,6 @@ setup(
     zip_safe=True,
     scripts=["bin/evrm"],
     long_description=read(),
-    package_dir={"": "lib"},
-    py_modules=["obj", "run"],
     packages=["evrm"],
     include_package_data=True,
     data_files=[("share/doc/evrm", uploadlist("docs"))],
