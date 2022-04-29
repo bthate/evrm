47a69f748e758bb9423c8e07ce17dc5cb38c0870
Bart Thate <bthate67@gmail.com>
2022-04-29 15:12:04 +0200

    geen install require

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-# OTP-CR-117/19 otp.informationdesk@icc-cpi.int http://pypi.org/project/genocide
-#
 # This file is placed in the Public Domain.
+
 
 import os
 
+
 from setuptools import setup
+
 
 def uploadlist(dir):
     upl = []
@@ -22,18 +23,20 @@ def uploadlist(dir):
             upl.append(d)
     return upl
 
+
 def read():
     return open("README.rst", "r").read()
 
+
 setup(
     name='evrm',
-    version='100',
+    version='101',
     url='https://github.com/bthate/evrm',
     author='Bart Thate',
     author_email='bthate67@gmail.com',
     description="ANTIPSYCHOTICA - AKATHISIA - KATATONIE - SEDERING - SHOCKS - LETHALE KATATONIE !!!",
     license='Public Domain',
-    zip_safe=True,
+    zip_safe=False,
     scripts=["bin/evrm"],
     long_description=read(),
     packages=["evrm"],
